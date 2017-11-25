3cebe1c03a3593d6b979ff45e9d8a0fa128b8602
Edmar Dausan <edmardausan15@gmail.com>
2017-11-25 12:35:27 +0800

    deleting Docs folder

--- docs/goldelox.php
+++
@@ -1,5 +0,0 @@
-<?php include('header.php') ?>
-
-    <h1>Goldelox Graphics Processor</h1>
-
-<?php include('footer.php') ?>
--- docs/picaso-lite.php
+++
@@ -1,5 +0,0 @@
-<?php include('header.php') ?>
-
-    <h1>Picaso-Lite Graphics Processor</h1>
-
-<?php include('footer.php') ?>
