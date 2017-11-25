0817159f65ca4dd7bee9d84c8b3aab09c218fbaa
Edmar Dausan <edmardausan15@gmail.com>
2017-11-25 14:02:33 +0800

    added the main header

---
+++ main_header.php
@@ -0,0 +1,191 @@
+<!DOCTYPE html>
+<html style="" class=" js csstransforms3d csstransitions" lang="en">
+
+<head>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>Home | 4D Labs</title>
+    <meta name="description" content="4D Labs designs and manufactures compact and cost effective Intelligent Display Modules using the latest state of the art OLED and LCD technology with an embedded custom graphics processor.">
+    <meta name="keywords" content="4d labs, chipset solutions,
+chipset,
+solutions,
+canned modules,
+modules
+graphics solutions,
+graphics,
+development resources,
+development,
+application notes,
+application,
+micro7-LCD,
+micro7,
+LCD,
+audio,
+touch,
+wireless,
+embedded graphics controllers,
+embedded graphics,
+embedded,
+OLED">
+    <!--<meta name="robots" content="INDEX,FOLLOW" />-->
+    <meta name="robots" content="http://4dlabs.com.au/robots.txt" <!--google="" verifications--="">
+    <meta name="google-site-verification" content="21-gP70Lk3S13PoQqSxUULn6wIsY6VjPuKThHll18Ko">
+    <!-- bing verifications-->
+    <meta name="msvalidate.01" content="7E8CE654396F64705942C93C3E35CEC6">
+
+    <link rel="icon" href="http://4dlabs.com.au/media/favicon/default/4DLabs_favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="http://4dlabs.com.au/media/favicon/default/4DLabs_favicon.ico" type="image/x-icon">
+    <!--[if lt IE 7]>
+<script type="text/javascript">
+//<![CDATA[
+    var BLANK_URL = 'http://4dlabs.com.au/js/blank.html';
+    var BLANK_IMG = 'http://4dlabs.com.au/js/spacer.gif';
+//]]>
+</script>
+<![endif]-->
+    <script async="" src="default-js/analytics.js"></script>
+    <script type="text/javascript" async="" src="default-js/ga.js"></script>
+    <script language="javascript" src="default-js/jquery-latest.js"></script>
+    <style></style>
+    <script>
+        var $ = jQuery.noConflict();
+    </script>
+    <script type="text/javascript" src="default-js/jquery_002.js"></script>
+
+    <link rel="stylesheet" type="text/css" href="default-css/styles.css" media="all">
+    <link rel="stylesheet" type="text/css" href="default-css/widgets.css" media="all">
+    <link rel="stylesheet" type="text/css" href="default-css/style.css" media="all">
+    <link rel="stylesheet" type="text/css" href="default-css/layout.css" media="all">
+    <link rel="stylesheet" type="text/css" href="default-css/pages.css" media="all">
+    <link rel="stylesheet" type="text/css" href="default-css/colorbox.css" media="all">
+    <link rel="stylesheet" type="text/css" href="default-css/jquery_002.css" media="all">
+    <link rel="stylesheet" type="text/css" href="default-css/ub.css" media="all">
+    <link rel="stylesheet" type="text/css" href="default-css/owl.css" media="all">
+    <link rel="stylesheet" type="text/css" href="default-css/easytabs.css" media="all">
+    <link rel="stylesheet" type="text/css" href="default-css/print.css" media="print">
+    <link rel="stylesheet" type="text/css" href="default-css/flexslider.css" media="screen">
+    <!-- <link rel="stylesheet" type="text/css" href="css/style.css" media="all"> -->
+
+
+    <!-- Custom Styles -->
+    <link rel="stylesheet" href="css/dev-style.min.css">
+
+    <script type="text/javascript" src="default-js/prototype.js"></script>
+    <script type="text/javascript" src="default-js/ccard.js"></script>
+    <script type="text/javascript" src="default-js/validation.js"></script>
+    <script type="text/javascript" src="default-js/builder.js"></script>
+    <script type="text/javascript" src="default-js/effects.js"></script>
+    <script type="text/javascript" src="default-js/dragdrop.js"></script>
+    <script type="text/javascript" src="default-js/controls.js"></script>
+    <script type="text/javascript" src="default-js/slider.js"></script>
+    <script type="text/javascript" src="default-js/js.js"></script>
+    <script type="text/javascript" src="default-js/form.js"></script>
+    <script type="text/javascript" src="default-js/menu.js"></script>
+    <script type="text/javascript" src="default-js/translate.js"></script>
+    <script type="text/javascript" src="default-js/cookies.js"></script>
+    <script type="text/javascript" src="default-js/jquery_004.js"></script>
+    <script type="text/javascript" src="default-js/general.js"></script>
+    <script type="text/javascript" src="default-js/modernizr.js"></script>
+    <script type="text/javascript" src="default-js/highlight.js"></script>
+    <script type="text/javascript" src="default-js/SpryTabbedPanels.js"></script>
+    <script type="text/javascript" src="default-js/jquery_006.js"></script>
+    <script type="text/javascript" src="default-js/jquery_003.js"></script>
+    <script type="text/javascript" src="default-js/jquery.js"></script>
+    <script type="text/javascript" src="default-js/owl.js"></script>
+    <script type="text/javascript" src="default-js/easytabs.js"></script>
+    <script type="text/javascript" src="js/jquery.min.js"></script>
+
+
+    <script src="scripts/script.js"></script>
+    <!--[if lt IE 8]>
+    <link rel="stylesheet" type="text/css" href="http://4dlabs.com.au/skin/frontend/default/default/css/styles-ie.css" media="all" />
+    <![endif]-->
+        <!--[if lt IE 7]>
+    <script type="text/javascript" src="http://4dlabs.com.au/js/lib/ds-sleight.js"></script>
+    <script type="text/javascript" src="http://4dlabs.com.au/skin/frontend/base/default/js/ie6.js"></script>
+    <![endif]-->
+        <!--[if lt IE 9]>
+    <script type="text/javascript" src="http://4dlabs.com.au/skin/frontend/base/default/js/clone/respond.js"></script>
+    <script type="text/javascript" src="http://4dlabs.com.au/skin/frontend/base/default/js/clone/html5.js"></script>
+    <![endif]-->
+
+    <script type="text/javascript">
+        //<![CDATA[
+        Mage.Cookies.path = '/';
+        Mage.Cookies.domain = '.4dlabs.com.au';
+        //]]>
+    </script>
+    <link media="all" rel="stylesheet" type="text/css" href="default-css/font-awesome.css">
+    <link rel="stylesheet" href="default-css/geogrotesque-normal.css" type="text/css" media="all">
+    <script type="text/javascript" src="default-js/jquery_005.js"></script>
+    <link rel="stylesheet" href="default-css/jquery.css">
+
+    <script type="text/javascript">
+        //<![CDATA[
+        optionalZipCountries = ["HK", "IE", "MO", "PA"];
+        //]]>
+    </script>
+    <!-- BEGIN GOOGLE ANALYTICS CODEs -->
+    <script type="text/javascript">
+        //<![CDATA[
+        var _gaq = _gaq || [];
+
+        _gaq.push(['_setAccount', 'UA-92058213-1']);
+        _gaq.push(['_gat._anonymizeIp']);
+        _gaq.push(['_trackPageview']);
+
+        (function () {
+            var ga = document.createElement('script');
+            ga.type = 'text/javascript';
+            ga.async = true;
+            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') +
+                '.google-analytics.com/ga.js';
+            var s = document.getElementsByTagName('script')[0];
+            s.parentNode.insertBefore(ga, s);
+        })();
+
+        //]]>
+    </script>
+    <!-- END GOOGLE ANALYTICS CODE -->
+    <script type="text/javascript">
+        //<![CDATA[
+        var Translator = new Translate([]);
+        //]]>
+    </script>
+
+    <script>
+        (function (i, s, o, g, r, a, m) {
+            i['GoogleAnalyticsObject'] = r;
+            i[r] = i[r] || function () {
+                (i[r].q = i[r].q || []).push(arguments)
+            }, i[r].l = 1 * new Date();
+            a = s.createElement(o),
+                m = s.getElementsByTagName(o)[0];
+            a.async = 1;
+            a.src = g;
+            m.parentNode.insertBefore(a, m)
+        })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+
+        ga('create', 'UA-92058213-1', 'auto');
+        ga('send', 'pageview');
+    </script>
+</head>
+
+<body class=" homepage  cms-index-index cms-home">
+
+    <div id="wrapper">
+        <div id="inner-wrapper" style="padding-top: '+xxx+'px !important">
+            <noscript>
+                <div class="global-site-notice noscript">
+                    <div class="notice-inner">
+                        <p>
+                            <strong>JavaScript seems to be disabled in your browser.</strong>
+                            <br /> You must have JavaScript enabled in your browser to utilize the functionality of this website.
+                            </p>
+                    </div>
+                </div>
+            </noscript>
+
+
+<?php include('navbar_1.php'); ?>
