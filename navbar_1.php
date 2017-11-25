79f507966544c708db90f001e49ca52fe350c8a7
Edmar Dausan <edmardausan15@gmail.com>
2017-11-25 14:09:36 +0800

    added navbar_1 content

---
+++ navbar_1.php
@@ -0,0 +1,284 @@
+<header id="header" class="page-header">
+    <div class="section-wrapper top-bg">
+        <nav class="col-full" role="navigation">
+
+            <a href="index.php" title="4D Labs" id="logo"></a>
+
+            <div class="site-header">
+                <h1 class="site-title">
+                    <a href="http://4dlabs.com.au/" title="4D Labs">4D Labs</a>
+                </h1>
+                <h2 class="site-description"></h2>
+            </div>
+
+            <div class="top-navigation">
+
+                <!-- Nav toggle -->
+                <div class="nav-toggle" >
+                    <i class="fa fa-bars"></i>
+                    <a href="#navigation">
+                        <span>Navigation</span>
+                    </a>
+                </div>
+
+                <ul id="top-nav" class="nav">
+                    <li class="first">
+                        <a href="http://4dlabs.com.au/customer/account/" title="My Account">Account</a>
+                    </li>
+                    <li>
+                        <a href="http://4dlabs.com.au/news" title="News">News</a>
+                    </li>
+                    <li>
+                        <a href="http://4dlabs.com.au/checkout/cart/" title="Cart" class="top-link-cart">Cart</a>
+                    </li>
+                    <li class=" last" id="login-link">
+                        <a href="http://4dlabs.com.au/customer/account/login/" title="Login">Login</a>
+                    </li>
+                </ul>
+
+                <!-- Search bar -->
+                <div class="search_main fix">
+
+                    <div class="google-search">
+
+                        <span id="global-search-option-type">
+                            <i class="fa fa-search"></i>
+                        </span>
+                        <ol id="global-search-option-list" style="display: none;">
+                            <li data-id="26">Products</li>
+                            <li data-id="27">Appnotes</li>
+                        </ol>
+                        <form name="cse-search-box" id="cse-search-box" class="searchform" action="http://4dlabs.com.au/catalogsearch/result/" method="get">
+
+                            <input id="" name="q" maxlength="128" size="26" class="field s" placeholder="Search for Products" type="text">
+
+
+                            <input name="cat" value="26" id="global-search-option" type="hidden">
+                            <input name="sa" value="Search" class="submits" type="submit">
+                            <div id="search_autocomplete" class="search-autocomplete"></div>
+
+                            <script type="text/javascript">
+                                //<![CDATA[
+                                //var searchForm = new Varien.searchForm('cse-search-box', 'search', 'Search entire store here...');
+                                //searchForm.initAutocomplete('http://4dlabs.com.au/catalogsearch/ajax/suggest/', 'search_autocomplete');
+                                //]]>
+                            </script>
+
+                        </form>
+                    </div>
+                    <!--/Google CSE-->
+                </div>
+                
+
+                <!-- Mobile Tabs -->
+                <ul class="mobile-tab">
+                    <li>
+                        <a href="">
+                            <i class="fa fa-user"></i>
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="">
+                            <i class="fa fa-newspaper-o"></i>
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="">
+                            <i class="fa fa-shopping-cart"></i>
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="">
+                            <i class="fa fa-sign-in"></i>
+                        </a>
+                    </li>
+                </ul>
+
+                
+
+            </div>
+            <!--/.top-navigation-->
+
+        </nav>
+        <div class="clear"></div>
+    </div>
+    <div class="section-wrapper nav-wrapper">
+
+        <!-- Navbar -->
+        <div class="col-full nav">
+            <nav id="navigation" role="navigation">
+                <section class="menus">
+                    <ul id="menu-nav" class="nav">
+
+                        <li class="level0 nav-1 first level-top parent">
+                            <a href="http://4dlabs.com.au/products.html" class="level-top" id="products">Products
+                                <span></span>
+                            </a>
+
+                        </li>
+
+                        <li class="level0 nav-2 level-top">
+                            <a href="http://4dlabs.com.au/appnotes.html" class="level-top" id="appnotes">App Notes
+                                <span></span>
+                            </a>
+                        </li>
+
+                        <li class="level0 nav-3 level-top">
+                            <a href="http://4dlabs.com.au/distributors.html" class="level-top" id="distributors">Distributors
+                                <span></span>
+                            </a>
+                        </li>
+
+                        <li class="level0 nav-4 level-top">
+                            <a href="http://4dlabs.com.au/about-us.html" class="level-top" id="about">About Us
+                                <span></span>
+                            </a>
+                        </li>
+
+                        <li class="level0 nav-5 last level-top">
+                            <a href="http://4dlabs.com.au/contact-us.html" class="level-top" id="contact">Contact Us
+                                <span></span>
+                            </a>
+                        </li>
+
+                    </ul>
+                </section>
+                
+
+                <!--/.menus-->
+
+                <!--/.menus-->
+
+                <a href="#top" class="nav-close">
+                    <span>Return to Content</span>
+                </a>
+            </nav>
+            <!-- /#navigation -->                                         
+
+        </div>
+
+        <!-- /.col-full -->
+
+        <div class="clear"></div>
+
+    </div>
+
+    <!-- Submenus -->
+    <div class="sub-menu hiddenSub">
+        
+        <ul class="submenu-list hideSubMenuContent" id="products">
+            <h2>Products</h2>
+            <!-- Overview -->
+            <li class="submenu-item ">
+                <label for="">Overview</label>
+                <div class="overview-wrapper">
+                    <a href="">
+                        <img src="img/goldelox.png" alt="">
+                    </a>
+                </div>
+            </li>
+            
+            <!-- Processors -->
+            <li class="submenu-item center">
+                <label for=""><a href="http://">Processors</a></label>
+                
+                <ul>
+                    <li>
+                        
+                        <a href="goldelox.php" id="goldelox"><i class="fa fa-microchip" ></i> Goldelox</a>
+                    </li>
+                    <li>
+                        <a href="picaso.php" id="picaso"><i class="fa fa-microchip" ></i> Picaso</a>
+                    </li>
+                    <li>
+                        <a href="diablo16.php" id="diablo16"><i class="fa fa-microchip" ></i> Diablo16</a>
+                    </li>
+                    <li>
+                        <a href="picaso-lite.php" id="picaso-Lite"><i class="fa fa-microchip" ></i> Picaso-Lite</a>
+                    </li>
+                </ul>
+            </li>
+
+            <li class="submenu-item">
+                <label for=""><a href="">Development Tools</a></label>
+                
+
+                <ul>
+                    <li>
+                        <a href="http://" id="workshop4"><i class="fa fa-window-maximize"></i> Workshop4 PRO IDE</a>
+                    </li>
+                </ul>
+                
+            </li>
+        
+        </ul>
+
+
+        <ul class="submenu-list hideSubMenuContent" id="appnotes">
+            <h2>App Notes</h2>
+            <!-- Overview -->
+            <li class="submenu-item ">
+                <label for="">Environment</label>
+                <ul>
+                    <li><a href=""><img src="img/designer.png" alt="">  Designer</a></li>
+                    <li><a href=""><img src="img/serial.png" alt=""> Serial</a></li>
+                    <li><a href=""><img src="img/visi.png" alt=""> ViSi</a></li>
+                    <li><a href=""><img src="img/visigenie.png" alt=""> ViSi Genie</a></li>
+                </ul>   
+            </li>
+            
+            <!-- Processors -->
+            <li class="submenu-item center">
+                <label for="">Processors</label>
+                
+                <ul>
+                    <li>
+                        <a href="goldelox.php" id="goldelox"><i class="fa fa-microchip" ></i> Goldelox</a>
+                    </li>
+                    <li>
+                        <a href="picaso.php" id="picaso"><i class="fa fa-microchip" ></i> Picaso</a>
+                    </li>
+                    <li>
+                        <a href="diablo16.php" id="diablo16"><i class="fa fa-microchip" ></i> Diablo16</a>
+                    </li>
+                    <li>
+                        <a href="picaso-lite.php" id="picaso-Lite"><i class="fa fa-microchip" ></i> Picaso-Lite</a>
+                    </li>
+                </ul>
+            </li>
+
+            <li class="submenu-item">
+                <label for="">Difficulty</label>
+                
+
+                <ul>
+                    <li><a href=""><i class="fa fa-bar-chart" ></i> Easy</a></li>
+                    <li><a href=""><i class="fa fa-bar-chart" ></i> Average</a></li>
+                    <li><a href=""><i class="fa fa-bar-chart" ></i> Difficult</a></li>
+                </ul>
+                
+            </li>
+        
+        </ul>
+
+
+        <ul class="submenu-list hideSubMenuContent" id="distributors">
+            <h2>Distributors</h2>
+            <li></li>
+        </ul>
+
+        <ul class="submenu-list hideSubMenuContent" id="about">
+            <h2>About Us</h2>
+            <p>4D Labs Semiconductors is a global leader in the research, development and manufacture of intelligent graphics solutions, offering customers unique, innovative, and cost effective yet market leading graphics processors, catering for a range of applications and industries. <a href="">Read More</a></p> 
+        </ul>
+
+        <ul class="submenu-list hideSubMenuContent" id="contact">
+            <h2>Contact Us</h2>
+            <li></li>
+        </ul>
+    </div>  
+    
+</header>
