832782893216bd5b525560c401d28b216af59614
Edmar Dausan <edmardausan15@gmail.com>
2017-11-29 15:37:01 +0800

    Rename some folders

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php include('headers/header-2.php'); ?>
-<?php include('parts/banner.html'); ?>
-<?php include('parts/featured.html'); ?>
-<?php include('parts/quick-links.html'); ?>
-<?php include('parts/news.html'); ?>
+<?php include('Include/banner.html'); ?>
+<?php include('Include/featured.html'); ?>
+<?php include('Include/quick-links.html'); ?>
+<?php include('Include/news.html'); ?>
 <?php include('footers/footer-2.php'); ?>
