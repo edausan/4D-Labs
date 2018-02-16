e1a1643328781cc8086482917bec6bf9d82b81eb
Edmar Dausan <edmardausan15@gmail.com>
2018-02-16 14:12:06 +0800

    appnotes home

--- all.php
+++ all.php
@@ -2,13 +2,11 @@
     define('TITLE', 'Home');
     include('header-2.php');
     include('include/banner.php');
-    // include('include/featured.html');
     include('include/highlights.php');
     include('include/quick-links.html');
-    // include('include/thumbnails-2.html');
     include('include/thumbnails.php');
+    include('include/appnotes-home.php');
     include('include/news.php');
-    // include('include/news.html');
-    include('include/discover.php');
+    // include('include/newsletter-home.php');
     include('footer-3.php');
 ?>
---
+++ include/newsletter-home.php
@@ -0,0 +1,16 @@
+<section class="lab-container-fluid">
+
+    <article class="lab-row">
+    
+        <section class="newsletter-home-wrapper">
+            <article class="lab-container">
+                <section class="lab-row">
+                    <p class="fleft">Sign up for our weekly newsletter to get the latest technique, tips and educational content.</p>
+                    <button class="fright"><i class="fa fa-envelope"></i> Subscribe</button>
+                </section>
+            </article>
+        </section>
+
+    </article>
+
+</section>
