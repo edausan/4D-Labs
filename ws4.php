c5e19d9a7ea77c422f88637f2293f7cbdcbe5cca
Edmar Dausan <edmardausan15@gmail.com>
2018-03-26 08:11:51 +0800

    workshop4 product page revision

--- ws4.php
+++ ws4.php
@@ -8,9 +8,19 @@
 
                 <img src="img/workshop4.png" alt="" class="mobile-product-view">
 
-                <p>Perpetual research and development drives evolutions in our hardware solutions. Our proprietary WS4 software has been uniquely developed to provide engineers with a powerful utility for rapid prototype, development, and deployment of solutions into whatever scenario you have planned.</p>
+                <div class="workshop">
+                    <p>Perpetual research and development drives evolutions in our hardware solutions. Our proprietary WS4 software has been uniquely developed to provide engineers with a powerful utility for rapid prototype, development, and deployment of solutions into whatever scenario you have planned.</p>
 
+                    <p>Workshop4 is a comprehensive software IDE for Microsoft Windows that provides an integrated software development platform for all of the 4D family of processors and modules. The IDE combines the Editor, Compiler, Linker and Downloader to develop complete 4DGL application code. All user application code is developed within the Workshop4 IDE.</p>
+                </div>
                 <figure class="ws4-content"><a href="http://www.4dsystems.com.au/product/4D_Workshop_4_IDE/" rel="noopener noreferrer" target="_blank" title="Workshop4 PRO IDE - Download Now!"><img src="img/about-banners/ws4-content.png" alt="Workshop4 PRO IDE - Download Now!"></a></figure>
+
+                <div class="workshop compatibility-wrapper">
+                    <p>Compatibility: <br/> Workshop4 is a Windows Only product.</p>
+                    <p>It is compatible with Windows XP through to Windows 10. Note some older OS's such as ME and Vista have not been tested for some time due to availability, however should still work. Obviously Windows 7+ is ideal.</p>
+                    <p>It is possible to run Workshop4 under a VM on Linux or Mac, however it is up to the User to set this up.</p>
+                </div>
+                
             </section>
 
             <section class="workshop workshop-environments-wrapper">
@@ -80,6 +90,166 @@
                     <li><i class="fa fa-chevron-right"></i> <span>The output frames can be utilized using any of the supported Workshop4 environments.</span> </li>
                 </ul>
 
+            </section>
+
+            <section class="workshop genie-magic-wrapper">
+                <h1>Genie Magic</h1>
+
+                <div class="genie-description-wrapper">
+                    <p>Genie Magic is an extension to ViSi-Genie, which is available in Workshop4 PRO. Genie Magic allows the User to add 4DGL code to various points in the ViSi-Genie backend code, which allows Users to take full advantage of the power of 4DGL, while retaining the speed and convenience of ViSi-Genie and its defined communication protocol.</p>
+
+                    <p>Genie Magic allows you to enter 4DGL code in the form of Events, Code and Objects, along with various touch related additions too, giving you to the power to achieve advanced GUI's in ViSi-Genie without having to write the full code yourself.</p>
+
+                    <p>Genie Magic is fully supported by the genieArduino and geniePi libraries, making communication with an Arduino or Raspberry Pi host even more powerful. </p>
+
+                    <p>When using Genie Magic in combination with the Smart Widget Editor, it is possible to use the Smart Widgets Editor tool and Smart Widgets to create other graphical user interface elements such as: </p>
+                </div>
+
+                <ul>
+                    <li>dropdown menu</li>
+                    <li>side menu</li>
+                    <li>'carousel slider'</li>
+                    <li>list box</li>
+                    <li>quarter-rotary menu</li>
+                    <li>dock menu</li>
+                    <li>fading slide-to-unlock button</li>
+                    <li>spectrum with transparency</li>
+                    <li>and many others</li>
+                </ul>
+            </section>
+
+            <section class="workshop announcements-wrapper">
+                <h1>Announcements</h1>
+
+                <div class="announcements">
+                    <p>The Workshop4 PRO License unlocks Genie Magic for the ViSi-Genie environment, along with the Smart Widget Editor for both ViSi and ViSi-Genie. New Features and additions continue to be developed, and updates are made available for free once the License has been purchased. </p>
+
+                    <p>Workshop4 Pro's addition to ViSi-Genie offers additional and specialised features, which has an encompassing title of Genie Magic. Genie Magic brings the ability to add standard 4DGL code to various points within the ViSi-Genie environment. This enables you to have all the advantages of ViSi-Genie but with the new ability to add extra 4DGL where you want it. If a feature was previously unavailable in standard ViSi-Genie but was possible in Designer or ViSi, then Workshop4 PRO is the simple answer, making the most powerful yet easy to use environment we offer.</p>
+
+                    <p>In addition to this, the Smart Widget Editor brings a comprehensive tool which allows you to create and animate Gauges, Sliders, Knobs and more, using the tools provided. This allows you to easily add custom graphics and build customised widgets, exactly as you want them.</p>
+
+                    <p>These features are available to be unlocked by purchasing a License for Workshop4 PRO, from the Buy Now section at the bottom of this page.</p>
+
+                    <p>A seven-day trial of Workshop4 PRO features is available by clicking on the 'Start PRO Trial' button in the Options Menu under the License tab in Workshop4 PRO. This will allow you to evaluate Workshop4 PRO unhindered for 7 different days of use - they do not have to be consecutive.</p>
+                    
+                    <p>With the aid of Workshop4 PRO, ViSi-Genie is an incredible environment to work with, giving many options to the User which extended ViSi-Genie beyond the Host Dependent platform it once was.</p>
+
+                    <p>Workshop4 PRO is an OPTIONAL upgrade to unlock advanced features. Workshop4 is still FREE to download and use, however, the PRO features requires a paid license to unlock the additional features.</p>
+
+                    <p>The Workshop4 PRO License is for One (1) computer only. </p>
+                </div>
+            </section>
+
+            <section class="workshop product-video-wrapper">
+                <h1>Product Videos</h1>
+
+                <div class="prod-video-wrapper">
+                    <iframe src="https://www.youtube.com/embed/81cez-Ws4dQ" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+                    <iframe src="https://www.youtube.com/embed/w1PCTq9H3Dw" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+                    <iframe src="https://www.youtube.com/embed/hSvNnPwSxnE" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+                    <iframe src="https://www.youtube.com/embed/HQdGWwzr7Bw" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+                </div>
+            </section>
+
+
+            <section class="workshop documentation">
+                        <h1>Workshop4 General Documentation</h1>
+                        <ul>
+                            <li>
+                                <a href="http://www.4dsystems.com.au/downloads/Software/4D-Workshop4-IDE/Workshop4-ReleaseNotes.txt">
+                                    <figure><img src="https://www.4dsystems.com.au/images/icons/icon_dl_txt_50x50.png" alt=""></figure>
+                                    <label for="">Workshop4 IDE Release <br/> Notes</label>
+                                    
+                                </a>
+                            </li>
+                            <li>
+                                <a href="http://www.4dsystems.com.au/productpages/4D-Workshop-4-IDE/downloads/Workshop-4_userguide_R_2_1.pdf">
+                                    <figure><img src="https://www.4dsystems.com.au/images/icons/icon_dl_pdf_50x50.png" alt=""></figure>
+                                    <label for="">Workshop4 IDE <br/> User Guide <span>Rev. 2.1 | 3.14 MB</span></label>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="http://www.4dsystems.com.au/productpages/4D-Workshop-4-IDE/downloads/4D-Workshop-4-IDE_installguide_R_2_0.pdf">
+                                    <figure><img src="https://www.4dsystems.com.au/images/icons/icon_dl_pdf_50x50.png" alt=""></figure>
+                                    <label for="">Workshop4 IDE Installation Guide<span>Rev. 2.0 | 939.22 KB</span></label>
+                                </a>
+                            </li>
+                        </ul>
+
+                        <h1>Workshop4 Pro Documentation</h1>
+
+                        <ul>
+                            <li>
+                                <a href="https://www.4dsystems.com.au/productpages/4D-Workshop-4-IDE/downloads/4DLS-MN-SWE-UGS_R_1_0.pdf">
+                                    <figure><img src="https://www.4dsystems.com.au/images/icons/icon_dl_pdf_50x50.png" alt=""></figure>
+                                    <label for="">Smart Widgets User Guide<span>Rev. 1.0 | 3.40 MB</span></label>
+                                </a>
+                            </li>
+                        </ul>
+
+                        <h1>Designer Environment Documentation</h1>
+
+                        <ul>
+                            <li>
+                                <a href="http://www.4dsystems.com.au/productpages/4DGL/downloads/4DGL_progmanual_R_6_0.pdf">
+                                    <figure><img src="https://www.4dsystems.com.au/images/icons/icon_dl_pdf_50x50.png" alt=""></figure>
+                                    <label for="">4DGL Programmer's Reference Manual<span>Rev. 6.0 | 1.48 MB</span></label>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="http://www.4dsystems.com.au/productpages/GOLDELOX/downloads/GOLDELOX_internalfunctions_R_7_0.pdf">
+                                    <figure><img src="https://www.4dsystems.com.au/images/icons/icon_dl_pdf_50x50.png" alt=""></figure>
+                                    <label for="">GOLDELOX Internal Functions Manual<span>Rev. 7.0 | 2.97 MB</span></label>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="http://www.4dsystems.com.au/productpages/PICASO/downloads/PICASO_internalfunctions_R_7_0.pdf">
+                                    <figure><img src="https://www.4dsystems.com.au/images/icons/icon_dl_pdf_50x50.png" alt=""></figure>
+                                    <label for="">PICASO Internal Functions Manual<span>Rev. 7.0 | 4.86 MB</span></label>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="http://www.4dsystems.com.au/productpages/DIABLO16/downloads/DIABLO16_internalfunctions_R_2_0.pdf">
+                                    <figure><img src="https://www.4dsystems.com.au/images/icons/icon_dl_pdf_50x50.png" alt=""></figure>
+                                    <label for="">DIABLO16 Internal Functions Manual<span>Rev. 2.0 | 6.98 MB</span></label>
+                                </a>
+                            </li>
+                        </ul>
+
+                        <h1>ViSi Environment Documentation</h1>
+
+                        <ul>
+                            <li>
+                                <a href="http://www.4dsystems.com.au/productpages/ViSi/downloads/ViSi_userguide_R_2_1.pdf">
+                                    <figure><img src="https://www.4dsystems.com.au/images/icons/icon_dl_pdf_50x50.png" alt=""></figure>
+                                    <label for="">ViSi User Guide<span>Rev. 2.1 | 3.91 MB</span></label>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="http://www.4dsystems.com.au/productpages/4DGL/downloads/4DGL_progmanual_R_6_0.pdf">
+                                    <figure><img src="https://www.4dsystems.com.au/images/icons/icon_dl_pdf_50x50.png" alt=""></figure>
+                                    <label for="">4DGL Programmer's Reference Manual<span>Rev. 6.0 | 1.48 MB</span></label>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="http://www.4dsystems.com.au/productpages/GOLDELOX/downloads/GOLDELOX_internalfunctions_R_7_0.pdf">
+                                    <figure><img src="https://www.4dsystems.com.au/images/icons/icon_dl_pdf_50x50.png" alt=""></figure>
+                                    <label for="">GOLDELOX Internal Functions Manual<span>Rev. 7.0 | 2.97 MB</span></label>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="http://www.4dsystems.com.au/productpages/PICASO/downloads/PICASO_internalfunctions_R_7_0.pdf">
+                                    <figure><img src="https://www.4dsystems.com.au/images/icons/icon_dl_pdf_50x50.png" alt=""></figure>
+                                    <label for="">PICASO Internal Functions Manual<span>Rev. 7.0 | 4.86 MB</span></label>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="http://www.4dsystems.com.au/productpages/DIABLO16/downloads/DIABLO16_internalfunctions_R_2_0.pdf">
+                                    <figure><img src="https://www.4dsystems.com.au/images/icons/icon_dl_pdf_50x50.png" alt=""></figure>
+                                    <label for="">DIABLO16 Internal Functions Manual<span>Rev. 2.0 | 6.98 MB</span></label>
+                                </a>
+                            </li>
+                        </ul>
             </section>
             
         </article>
